34548306f0c49e559f5c47556e114d25f961ddc4
SudoTronic <61809199+SudoTronic@users.noreply.github.com>
2023-04-12 21:30:07 -0400

    Update restart_container.sh

--- Deployment/restart_container.sh
+++ Deployment/restart_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the name or ID of the container you want to restart
-CONTAINER_NAME_OR_ID="your_container_name_or_id"
+CONTAINER_NAME_OR_ID=a0d1d2516a3d5fb5999b0d22bdf901c6ddbc90e45232859cdcbda057aeba21b2
 
 # Stop the container
 docker stop $CONTAINER_NAME_OR_ID
